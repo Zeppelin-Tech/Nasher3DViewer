d41c19687830fd53cd95aa8f37125cf8cc6db651
Noah Olson <noah.v.olson@gmail.com>
2020-07-27 15:45:14 -0400

    Fetched json from URL and stored in javascript object

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,23 @@
 function main() {
     console.log("Hello World!");
+
+    let objectID = '11051';
+    let requestURL = 'https://emuseum.nasher.duke.edu/objects/' + objectID + '/json';
+
+    // Replace testURL with requestURL when whitelisted
+    testURL = 'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json';
+
+    let request = new XMLHttpRequest();
+    console.log(testURL);
+
+    request.open('GET', testURL);
+    request.responseType = 'json';
+    request.send();
+
+    request.onload = function() {
+        const relicData = request.response;
+        console.log(relicData);
+
+        // json data now stored in a javascript object
+    }
 }
