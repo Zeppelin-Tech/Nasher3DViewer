f58ff98398310907f4e85aa76e90dac9522e4244
Noah Olson <noah.v.olson@gmail.com>
2020-07-27 20:05:56 -0400

    Moved json request from client side to server side

--- app.js
+++ app.js
@@ -39,3 +39,24 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+///////////////////
+let objectID = '11051';
+let requestURL = 'https://emuseum.nasher.duke.edu/objects/' + objectID + '/json';
+
+// Replace testURL with requestURL when whitelisted
+testURL = 'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json';
+
+let request = new XMLHttpRequest();
+console.log(requestURL);
+
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+
+request.onload = function() {
+  const relicData = request.response;
+  console.log(relicData);
+
+  // json data now stored in a javascript object
+}
