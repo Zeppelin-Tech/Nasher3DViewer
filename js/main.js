c157cba6db8e4d137b10ad8b883ca4615437bf0c
expert700 <dyllancole@outlook.com>
2020-08-06 20:28:46 -0400

    Add logo in top left corner

--- js/main.js
+++ js/main.js
@@ -48,8 +48,16 @@ function main() {
         }
     }
 
+    initMaterializeComponents();
 
     window.switchModel = updateModel;
+}
+
+function initMaterializeComponents() {
+    document.addEventListener('DOMContentLoaded', function() {
+        let elems = document.querySelectorAll('.fixed-action-btn');
+        let instances = M.FloatingActionButton.init(elems, options);
+    });
 }
 
 
