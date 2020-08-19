01eebdba098b1bca14cebf63ead5335af6383c7b
Matthew Olson <molson18@punahou.edu>
2020-08-19 18:48:37 -0400

    changed scrollbar to use custom animations

--- js/main.js
+++ js/main.js
@@ -362,7 +362,7 @@ function hideModelScroll() {
         scrollBar.style.bottom = "0";
     }
 
-    animateCss(scrollBar, animation).then((message) => {
+    animateCss(scrollBar, animation, "scrollbar__").then((message) => {
         console.log(message);
         if (scrollBarHidden) {
             let modelBar = document.getElementById("models");
@@ -372,6 +372,7 @@ function hideModelScroll() {
     })
 }
 
+// This default to using animate.css built in animations, use your own by changing the prefix
 const animateCss = (node, animation, prefix = `animate__`) =>
     new Promise((resolve, reject) => {
         const animationName = `${prefix}${animation}`;
